e19aa110904074139d0f03b03a9d40dfac2ace14
fornari03 <221017023@aluno.unb.br>
2025-02-12 21:14:15 -0300

    feat: implementa o componente Main já com css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
-  return <Header />;  
+  // fragment (tags vazias)
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
