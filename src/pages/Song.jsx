b3c312299b7029ca529d5a3704ae24ff16db4f51
fornari03 <221017023@aluno.unb.br>
2025-02-13 15:01:32 -0300

    feat: implementa o player sem funcionalidade de tocar musica

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Player from "../components/Player";
+import { Link } from "react-router-dom";
 
 const Song = () => {
   return (
@@ -14,14 +15,14 @@ const Song = () => {
       </div>
 
       <div className="song__bar">
-        <div className="song__artist-image">
+        <Link to="/artist/1" className="song__artist-image">
           <img
             width={75}
             height={75}
             src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
             alt="Imagem do artista Y"
           />
-        </div>
+        </Link>
 
         <Player />
 
