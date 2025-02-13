3a1e55ac567335c543ad3cbc1b87ebae3455cf04
fornari03 <221017023@aluno.unb.br>
2025-02-13 14:40:44 -0300

    feat: implementa página de música sem o player

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Player from "../components/Player";
 
 const Song = () => {
   return (
-    <div>Song</div>
-  )
-}
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Imagem da música X"
+          />
+        </div>
+      </div>
 
-export default Song
+      <div className="song__bar">
+        <div className="song__artist-image">
+          <img
+            width={75}
+            height={75}
+            src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+            alt="Imagem do artista Y"
+          />
+        </div>
+
+        <Player />
+
+        <div>
+          <p className="song__name">Última saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
