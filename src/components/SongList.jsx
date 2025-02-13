4fa916cffa6491a30907d3bc6737be7640001796
fornari03 <221017023@aluno.unb.br>
2025-02-13 20:24:49 -0300

    feat: versão completa da página de artista

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { use } from "react";
 import SongItem from "../components/SongItem";
+import { useState } from "react";
 
 const SongList = ({ songsArray }) => {
-  const numberItems = 5;
+  const [numberItems, setItems] = useState(5);
   return (
     <div className="song-list">
-      {songsArray.filter((currentValue, index) => index < numberItems).map((song, index) => (
-        <SongItem key={index} index={index} {...song} />
-      ))}
-      <p className="song-list__see-more">Ver mais</p>
+      {songsArray
+        .filter((currentValue, index) => index < numberItems)
+        .map((song, index) => (
+          <SongItem key={index} index={index} {...song} />
+        ))}
+      <p
+        className="song-list__see-more"
+        onClick={() => {
+          setItems(numberItems + 5);
+        }}
+      >
+        Ver mais
+      </p>
     </div>
   );
 };
