db2a1c2cb455dfbd5cf219e3ba0c38ddfb9af664
fornari03 <221017023@aluno.unb.br>
2025-02-14 10:33:25 -0300

    feat: cria a conexão com o banco de dados mongoDB

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { artistArray } from "../../front-end/assets/database/artists.js";
-import { songsArray } from "../../front-end/assets/database/songs.js";
+import { db } from "./connect.js";
 
 const app = express();
 const PORT = 3000;
@@ -9,12 +8,12 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/artists", (req, res) => {
-  res.send(artistArray);
+app.get("/artists", async (req, res) => {
+  res.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", (req, res) => {
-  res.send(songsArray);
+app.get("/songs", async (req, res) => {
+  res.send(await db.collection("songs").find({}).toArray());
 });
 
 app.listen(PORT, () => {
