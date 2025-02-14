4da87b3e752dc3b73313ac0210411b2670247b6a
fornari03 <221017023@aluno.unb.br>
2025-02-14 13:48:02 -0300

    feat: implementa a progressBar da música

--- front-end/src/components/Player.jsx
+++ front-end/src/components/Player.jsx
@@ -7,16 +7,51 @@ import {
   faCirclePause,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+
+const formatTime = (timeInSecs) => {
+  const minutes = Math.floor(timeInSecs / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor(timeInSecs % 60)
+    .toString()
+    .padStart(2, "0");
+
+  return `${minutes}:${seconds}`;
+};
+
+const timeInSeconds = (timeString) => {
+  const [minutes, seconds] = timeString.split(":");
+  return parseInt(minutes) * 60 + parseInt(seconds);
+};
 
 const Player = ({ duration, songsArrayFromArtist, songIndex, audio }) => {
   const audioPlayer = useRef();
+  const progressBar = useRef();
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(formatTime(0));
+  const durationInSeconds = timeInSeconds(duration);
 
   const playPause = () => {
     isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
     setIsPlaying(!isPlaying);
+    setCurrentTime(formatTime(audioPlayer.current.currentTime));
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (isPlaying)
+        setCurrentTime(formatTime(audioPlayer.current.currentTime));
+
+      progressBar.current.style.setProperty("--_progress", `${(audioPlayer.current.currentTime / durationInSeconds) * 100}%`);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [isPlaying]);
+
+  const intervalId = setInterval(() => {
+    setCurrentTime(formatTime(audioPlayer.current.currentTime));
+  }, 1000);
 
   return (
     <div className="player">
@@ -48,10 +83,10 @@ const Player = ({ duration, songsArrayFromArtist, songIndex, audio }) => {
         </Link>
       </div>
       <div className="player__progress">
-        <p>00:00</p>
+        <p>{currentTime}</p>
 
         <div className="player__bar">
-          <div className="player__bar-progress"></div>
+          <div ref={progressBar} className="player__bar-progress"></div>
         </div>
 
         <p>{duration}</p>
